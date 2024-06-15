968865639966b9e5a01913291b518d1e7442be2e
Guerdy <guerdylambert9@gmail.com>
2024-06-15 13:33:22 -0400

    Header component first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header--div">
+            <div className='meme--div'>
+                <img src="./Troll-Face-Meme.webp" alt="Troll face"/>
+                <h2>Meme Generator</h2>
+            </div>
+            <h3 className='project--elem'>React Course - Project 3</h3>
+        </div>
+    )
+}
